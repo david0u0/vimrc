b8d248fe576eba2e9b75de6f1f9c93621818d306
why8ther <lturtsamuel@gmail.com>
2023-01-13 10:20:12 +0800

    Change install script to create language client list

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -6,6 +6,10 @@ cd ~/.vim_runtime
 echo 'set runtimepath+=~/.vim_runtime
 let g:my_format_on_save_list = []
 
+let g:LanguageClient_serverCommands = {
+            \ 'rust': ['rust-analyzer'],
+            \ }
+
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
